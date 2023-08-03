faabee4ddf62cd00ef7072b6f1961ab60fdd12fc
yalh76 <yalh@yahoo.com>
2023-08-03 23:41:56 +0200

    Fix pkgconf : Breaks: pkg-config

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="gawk icu-devtools libicu-dev libidn11-dev|libidn-dev liblua5.4-dev libncurses-dev libpkgconf3 libreadline-dev libsigsegv2 libssl-dev libyaml-dev lua-busted lua-cliargs lua-dkjson lua-expat lua-inifile lua-luassert lua-mediator lua-penlight lua-say lua-system lua-term lua-yaml lua5.4 pkg-config|pkgconf-bin pkgconf txt2man"
+pkg_dependencies="gawk icu-devtools libicu-dev libidn11-dev|libidn-dev liblua5.4-dev libncurses-dev libpkgconf3 libreadline-dev libsigsegv2 libssl-dev libyaml-dev lua-busted lua-cliargs lua-dkjson lua-expat lua-inifile lua-luassert lua-mediator lua-penlight lua-say lua-system lua-term lua-yaml lua5.4 pkg-config|pkgconf-bin txt2man"
 
 #=================================================
 # PERSONAL HELPERS
