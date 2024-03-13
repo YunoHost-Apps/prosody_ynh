02e7e8f4ee54cab619e47d5a19d3a24076c0f349
anubister <anubister@laposte.net>
2024-03-13 20:47:48 +0100

    Update _common.sh
    fix lua version

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,11 +4,12 @@
 # COMMON VARIABLES
 #=================================================
 
-if [[ $(lsb_release --codename --short) == "bookworm" ]]; then
-	luaversion="5.4"
-else
+#FIXME: does not work as manifest.toml lua5.4 dependencies have to be updated accordingly
+#if [[ $(lsb_release --codename --short) == "bookworm" ]]; then
+#	luaversion="5.4"
+#else
 	luaversion="5.2"
-fi
+#fi
 
 #=================================================
 # PERSONAL HELPERS
