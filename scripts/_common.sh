7fbc92897eb9a266b1b7749b2cc80f2ef5a4c7cf
pitchum <pitchum@users.noreply.github.com>
2025-01-08 19:35:13 +0100

    Hunt a lot of bugs in scripts (backup/restore/remove)

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,6 +4,10 @@
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
+app=prosody
+config_path=/etc/prosody
+
+
 #=================================================
 # PERSONAL HELPERS
 #=================================================
