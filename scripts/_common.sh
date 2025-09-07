8d1c7405f79cbb7bd25065c73176058ede56399d
eric_G <46165813+ericgaspar@users.noreply.github.com>
2025-09-07 19:43:02 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -12,7 +12,7 @@ _configure_prosody() {
     turn_password=$(ynh_app_setting_get --app="coturn" --key=turnserver_pwd)
     turn_external_port=$(ynh_app_setting_get --app="coturn" --key=port_turnserver_tls)
 
-    ynh_print_info "Adding prosody configuration files..."
+    ynh_print_info "Adding Prosody configuration files..."
 
     # Add 00.cfg.lua, needed to customize some settings acrros all prosody vhosts
     ynh_config_add --template="00.cfg.lua" --destination="/etc/prosody/conf.avail/00.cfg.lua"
@@ -28,14 +28,14 @@ _configure_prosody() {
     chmod 644 /var/www/.well-known/${domain}/host-meta
 
     # Add nginx config for xmpp subdomains
-    ynh_print_info "Configuring Nginx for extra domains (muc, pubsub, xmpp-upload, ...) and extra URLs (bosh, websocket)..."
+    ynh_print_info "Configuring NGINX for extra domains (muc, pubsub, xmpp-upload...) and extra URLs (bosh, websocket)..."
     ynh_config_add --template="nginx_prosody.conf" --destination="/etc/nginx/conf.d/${domain}.d/prosody.conf"
 
     cp -R "../conf/hook_conf_regen" "/usr/share/yunohost/hooks/conf_regen/98-nginx_$app"
     YNH_HELPERS_VERSION=1 yunohost tools regen-conf nginx
 
     # Create directory for file sharing uploads
-    ynh_print_info "Setting up http upload folder..."
+    ynh_print_info "Setting up HTTP upload folder..."
 
     mkdir -p "/var/xmpp-upload/${domain}/upload"
     chown -R prosody:www-data /var/xmpp-upload/
